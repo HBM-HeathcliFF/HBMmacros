fb9bca89f6ae59d927c25800b0f02d6b8623a7fb
HBM-HeathcliFF <rt.ru.i.abramov@yandex.ru>
2021-02-27 19:25:17 +0700

    Запретил запуск нескольких копий программы одновременно

--- HBM Macros/Program.cs
+++ HBM Macros/Program.cs
@@ -18,27 +18,29 @@ namespace HBMmacros
 
         [STAThread]
         static int Main (string[] args) {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            modifiers = new string[4];
-            keys = new int[4];
-
-            Form frm = new frmMain();
-            fHandle = frm.Handle;
-            frm.FormClosed += (s, e) => 
+            if (System.Diagnostics.Process.GetProcessesByName(Application.ProductName).Length == 1)
             {
-                for (int i = 0; i < 4; i++)
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                modifiers = new string[4];
+                keys = new int[4];
+
+                Form frm = new frmMain();
+                fHandle = frm.Handle;
+                frm.FormClosed += (s, e) =>
                 {
-                    UnregisterHotKey(fHandle, i);
-                }
-            };
-
-            using (NotifyIcon icon = new NotifyIcon())
-            {
-                icon.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
-                icon.ContextMenu = new ContextMenu(
-                    new[]
+                    for (int i = 0; i < 4; i++)
                     {
+                        UnregisterHotKey(fHandle, i);
+                    }
+                };
+
+                using (NotifyIcon icon = new NotifyIcon())
+                {
+                    icon.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+                    icon.ContextMenu = new ContextMenu(
+                        new[]
+                        {
                         new MenuItem("Autorun", (s, e) =>
                         {
                             const string name = "HBMmacros";
@@ -72,81 +74,82 @@ namespace HBMmacros
                             help.Show();
                         }),
                         new MenuItem("Exit", (s, e) => Application.Exit())
-                    });
-                try
-                {
-                    using (var key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\"))
+                        });
+                    try
                     {
-                        if ((string)key.GetValue("HBMmacros") == Application.ExecutablePath)
-                            icon.ContextMenu.MenuItems[0].Checked = true;
-                    }
-                }
-                catch (Exception)
-                {
-                    icon.ContextMenu.MenuItems[0].Checked = false;
-                }
-
-                try
-                {
-                    using (var key = Registry.CurrentUser.OpenSubKey("Software\\HBMmacros"))
-                    {
-                        for (int i = 0; i < 4; i++)
+                        using (var key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\"))
                         {
-                            modifiers[i] = key.GetValue($"Modifier{i}").ToString();
-                            keys[i] = (int)key.GetValue($"Key{i}");
+                            if ((string)key.GetValue("HBMmacros") == Application.ExecutablePath)
+                                icon.ContextMenu.MenuItems[0].Checked = true;
                         }
-                        delay = (int)key.GetValue("Delay");
-                        g1AmmoCount = (int)key.GetValue("g1AmmoCount");
-                        g2AmmoCount = (int)key.GetValue("g2AmmoCount");
-                        g3AmmoCount = (int)key.GetValue("g3AmmoCount");
-                        if ((int)key.GetValue("Heal") == 1) isHeal = true;
-                        else isHeal = false;
-                        if ((int)key.GetValue("Mheal") == 1) isMheal = true;
-                        else isMheal = false;
-                        if ((int)key.GetValue("Mask") == 1) isMask = true;
-                        else isMask = false;
-                        numberMask = (int)key.GetValue("NumberMask");
-                        isSaved = true;
                     }
-                }
-                catch (Exception)
-                {
-                    for (int i = 0, j = 49; i < 4; i++, j++)
+                    catch (Exception)
                     {
-                        modifiers[i] = "Alt";
-                        keys[i] = j;
+                        icon.ContextMenu.MenuItems[0].Checked = false;
                     }
-                    delay = 50;
-                    g1AmmoCount = 10;
-                    g2AmmoCount = 10;
-                    g3AmmoCount = 10;
-                    isHeal = true;
-                    isMheal = true;
-                    isMask = false;
-                    numberMask = 1;
-                    isSaved = false;
-                }
 
-                for (int i = 0; i < 4; i++)
-                {
-                    switch (modifiers[i])
+                    try
                     {
-                        case "Alt":
-                            RegisterHotKey(fHandle, i, Modifiers.ALT, (Keys)keys[i]);
-                            break;
-                        case "Ctrl":
-                            RegisterHotKey(fHandle, i, Modifiers.CONTROL, (Keys)keys[i]);
-                            break;
-                        case "Shift":
-                            RegisterHotKey(fHandle, i, Modifiers.SHIFT, (Keys)keys[i]);
-                            break;
+                        using (var key = Registry.CurrentUser.OpenSubKey("Software\\HBMmacros"))
+                        {
+                            for (int i = 0; i < 4; i++)
+                            {
+                                modifiers[i] = key.GetValue($"Modifier{i}").ToString();
+                                keys[i] = (int)key.GetValue($"Key{i}");
+                            }
+                            delay = (int)key.GetValue("Delay");
+                            g1AmmoCount = (int)key.GetValue("g1AmmoCount");
+                            g2AmmoCount = (int)key.GetValue("g2AmmoCount");
+                            g3AmmoCount = (int)key.GetValue("g3AmmoCount");
+                            if ((int)key.GetValue("Heal") == 1) isHeal = true;
+                            else isHeal = false;
+                            if ((int)key.GetValue("Mheal") == 1) isMheal = true;
+                            else isMheal = false;
+                            if ((int)key.GetValue("Mask") == 1) isMask = true;
+                            else isMask = false;
+                            numberMask = (int)key.GetValue("NumberMask");
+                            isSaved = true;
+                        }
                     }
-                }
+                    catch (Exception)
+                    {
+                        for (int i = 0, j = 49; i < 4; i++, j++)
+                        {
+                            modifiers[i] = "Alt";
+                            keys[i] = j;
+                        }
+                        delay = 50;
+                        g1AmmoCount = 10;
+                        g2AmmoCount = 10;
+                        g3AmmoCount = 10;
+                        isHeal = true;
+                        isMheal = true;
+                        isMask = false;
+                        numberMask = 1;
+                        isSaved = false;
+                    }
 
-                icon.Visible = true;
-                icon.ShowBalloonTip(100, "HBM Macros", "Приложение успешно запущено!", ToolTipIcon.None);
-                Application.Run();
-                icon.Visible = false;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        switch (modifiers[i])
+                        {
+                            case "Alt":
+                                RegisterHotKey(fHandle, i, Modifiers.ALT, (Keys)keys[i]);
+                                break;
+                            case "Ctrl":
+                                RegisterHotKey(fHandle, i, Modifiers.CONTROL, (Keys)keys[i]);
+                                break;
+                            case "Shift":
+                                RegisterHotKey(fHandle, i, Modifiers.SHIFT, (Keys)keys[i]);
+                                break;
+                        }
+                    }
+
+                    icon.Visible = true;
+                    icon.ShowBalloonTip(100, "HBM Macros", "Приложение успешно запущено!", ToolTipIcon.None);
+                    Application.Run();
+                    icon.Visible = false;
+                }
             }
             return 0;
         }
